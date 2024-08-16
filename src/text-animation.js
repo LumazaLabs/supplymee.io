aacd9a1c74e9622b9540c35752429c223e04b931
lukebricknell <lukecbricknell@gmail.com>
2024-08-16 20:22:30 +0200

    Removed paragraph animation

--- src/text-animation.js
+++ src/text-animation.js
@@ -37,125 +37,125 @@ document.addEventListener("DOMContentLoaded", (event) => {
   }
 });
 
-// SECTION ANIMATION
+// // SECTION ANIMATION
 
-document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.registerPlugin(ScrollTrigger);
+// document.addEventListener("DOMContentLoaded", (event) => {
+//   gsap.registerPlugin(ScrollTrigger);
 
-  // WHY SECTION
-  gsap.from("#why-h1", {
-    scrollTrigger: {
-      trigger: "#why-h1",
-      toggleActions: "play none none none",
-    },
-    y: 160,
-    duration: 1,
-    ease: "back",
-  });
+//   // WHY SECTION
+//   gsap.from("#why-h1", {
+//     scrollTrigger: {
+//       trigger: "#why-h1",
+//       toggleActions: "play none none none",
+//     },
+//     y: 160,
+//     duration: 1,
+//     ease: "back",
+//   });
 
-  gsap.from("#why-boxes div", {
-    scrollTrigger: {
-      trigger: "#why-boxes div",
-      toggleActions: "play none none none",
-    },
-    y: 160,
-    duration: 1,
-    ease: "back",
-    stagger: 0.1,
-  });
+//   gsap.from("#why-boxes div", {
+//     scrollTrigger: {
+//       trigger: "#why-boxes div",
+//       toggleActions: "play none none none",
+//     },
+//     y: 160,
+//     duration: 1,
+//     ease: "back",
+//     stagger: 0.1,
+//   });
 
-  // WHO SECTION
-  gsap.from("#who-section div", {
-    scrollTrigger: {
-      trigger: "#who-section div",
-      toggleActions: "play none none none",
-    },
-    y: 160,
-    duration: 1,
-    ease: "back",
-    stagger: 0.1,
-  });
+//   // WHO SECTION
+//   gsap.from("#who-section div", {
+//     scrollTrigger: {
+//       trigger: "#who-section div",
+//       toggleActions: "play none none none",
+//     },
+//     y: 160,
+//     duration: 1,
+//     ease: "back",
+//     stagger: 0.1,
+//   });
 
-  // HOW SECTION
-  gsap.from("#how-section div", {
-    scrollTrigger: {
-      trigger: "#how-section div",
-      toggleActions: "play none none none",
-    },
-    y: 160,
-    duration: 1,
-    ease: "back",
-    stagger: 0.1,
-  });
+//   // HOW SECTION
+//   gsap.from("#how-section div", {
+//     scrollTrigger: {
+//       trigger: "#how-section div",
+//       toggleActions: "play none none none",
+//     },
+//     y: 160,
+//     duration: 1,
+//     ease: "back",
+//     stagger: 0.1,
+//   });
 
-  // WHAT SECTION
-  gsap.from("#what-section div", {
-    scrollTrigger: {
-      trigger: "#what-section div",
-      toggleActions: "play none none none",
-    },
-    y: 160,
-    duration: 1,
-    ease: "back",
-    stagger: 0.1,
-  });
+//   // WHAT SECTION
+//   gsap.from("#what-section div", {
+//     scrollTrigger: {
+//       trigger: "#what-section div",
+//       toggleActions: "play none none none",
+//     },
+//     y: 160,
+//     duration: 1,
+//     ease: "back",
+//     stagger: 0.1,
+//   });
 
-  // COMPARISON SECTION
-  gsap.from("#comparison-section div", {
-    scrollTrigger: {
-      trigger: "#comparison-section div",
-      toggleActions: "play none none none",
-    },
-    y: 160,
-    duration: 1,
-    ease: "back",
-    stagger: 0.1,
-  });
+//   // COMPARISON SECTION
+//   gsap.from("#comparison-section div", {
+//     scrollTrigger: {
+//       trigger: "#comparison-section div",
+//       toggleActions: "play none none none",
+//     },
+//     y: 160,
+//     duration: 1,
+//     ease: "back",
+//     stagger: 0.1,
+//   });
 
-  // PRODUCT SECTION
-  gsap.from("#product-h1", {
-    scrollTrigger: {
-      trigger: "#product-h1",
-      toggleActions: "play none none none",
-    },
-    y: 160,
-    duration: 1,
-    ease: "back",
-    stagger: 0.1,
-  });
+//   // PRODUCT SECTION
+//   gsap.from("#product-h1", {
+//     scrollTrigger: {
+//       trigger: "#product-h1",
+//       toggleActions: "play none none none",
+//     },
+//     y: 160,
+//     duration: 1,
+//     ease: "back",
+//     stagger: 0.1,
+//   });
 
-  gsap.from("#product-1", {
-    scrollTrigger: {
-      trigger: "#product-1",
-      toggleActions: "play none none none",
-    },
-    y: 160,
-    duration: 1,
-    ease: "back",
-    stagger: 0.1,
-  });
+//   gsap.from("#product-1", {
+//     scrollTrigger: {
+//       trigger: "#product-1",
+//       toggleActions: "play none none none",
+//     },
+//     y: 160,
+//     duration: 1,
+//     ease: "back",
+//     stagger: 0.1,
+//   });
 
-  gsap.from("#product-2", {
-    scrollTrigger: {
-      trigger: "#product-1",
-      toggleActions: "play none none none",
-    },
-    y: 160,
-    duration: 1,
-    ease: "back",
-    stagger: 0.1,
-  });
+//   gsap.from("#product-2", {
+//     scrollTrigger: {
+//       trigger: "#product-1",
+//       toggleActions: "play none none none",
+//     },
+//     y: 160,
+//     duration: 1,
+//     ease: "back",
+//     stagger: 0.1,
+//   });
 
-  gsap.from("#cta-text", {
-    scrollTrigger: {
-      trigger: "#product-1",
-      toggleActions: "play none none none",
-    },
-    y: 160,
-    duration: 1,
-    ease: "back",
-    stagger: 0.1,
-  });
-});
+//   gsap.from("#cta-text", {
+//     scrollTrigger: {
+//       trigger: "#product-1",
+//       toggleActions: "play none none none",
+//     },
+//     y: 160,
+//     duration: 1,
+//     ease: "back",
+//     stagger: 0.1,
+//   });
+// });
 
-why - boxes;
+// why - boxes;
